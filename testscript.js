2b08fba54f2f6fa9fea4149a5a10d849a6bccb2a
atoneyd <31745478+atoneyd@users.noreply.github.com>
2021-04-28 12:35:00 -0400

    Added buttons

--- testscript.js
+++ testscript.js
@@ -1,9 +1,17 @@
 //Creates a new varibale called myItem that refers to element with the id "firsttest"
 myitem = document.getElementById("firsttest");
 
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
+thebutton2 = document.getElementById("thebutton2");  
+otheritem2 = document.getElementById("buttontest2");
+
 //Gives myItem an event listener, saying something should happen(execute onClick function) when it is clicked("click")
 myitem.addEventListener("click", onClick);
 myitem.addEventListener("mouseout", onMouseOut)
+
+thebutton.addEventListener("click", onButtonClick);
+thebutton2.addEventListener("click", onButtonClick2);
 
 //Tells myItem what to do when its clicked(change text color to green)
 function onClick() { //function to be executed when item is clicked
@@ -15,4 +23,10 @@ function onMouseOut() {
     myitem.style.color = "darkslateblue"; 
     myitem.style.backgroundColor = "seashell"; 
     myitem.style.borderStyle = "none"; 
+}
+function onButtonClick() {  
+    otheritem.style.color = "red";  
+}
+function onButtonClick2() {  
+    otheritem2.style.backgroundColor = "lightgrey";  
 }
